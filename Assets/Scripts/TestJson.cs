6ac9cfc1c079144a9a0e99c6823151a255ab4ca7
TSentler <centner_dc@mail.ru>
2025-12-24 14:43:50 +0500

    fix strip position

--- Assets/Scripts/TestJson.cs
+++ Assets/Scripts/TestJson.cs
@@ -13,7 +13,7 @@ namespace FightDojo
         [SerializeField] private GameObject keyTextPrefab;
         [SerializeField] private GameObject tabImagePrefab;
 
-        private Vector3 offset;
+        private Vector2 offset;
 
         private void Start()
         {
@@ -25,7 +25,7 @@ namespace FightDojo
 
         private void BuildComboStrip(RecordData recordData)
         {
-            offset = Vector3.zero;
+            offset = Vector2.zero;
             foreach (RecordedEvent recordedEvent in recordData.recorded_events_v2)
             {
                 BuildStripItem(recordedEvent, contentParent);
@@ -41,24 +41,26 @@ namespace FightDojo
         private void SpawnTabImage(RecordedEvent recordedEvent, Transform parent)
         {
             // 2. TabImage (интервал)
-            GameObject tabGO = Instantiate(tabImagePrefab, offset, Quaternion.identity, parent);
-            RectTransform tabRect = tabGO.GetComponent<RectTransform>();
+            GameObject tabGO = Instantiate(tabImagePrefab, parent);
+            RectTransform rect = tabGO.GetComponent<RectTransform>();
+            rect.anchoredPosition = offset;
 
             float widthPx = Mathf.Max(1f, recordedEvent.delay_ms * stripScale); // защита от 0
-            tabRect.sizeDelta = new Vector2(widthPx, tabRect.sizeDelta.y);
-            offset += Vector3.right * widthPx;
-
+            rect.sizeDelta = new Vector2(widthPx, rect.sizeDelta.y);
+            offset += Vector2.right * widthPx;
         }
 
         private void SpawnKeyText(RecordedEvent recordedEvent, Transform parent)
         {
             // 1. KeyText
-            RectTransform rect = keyTextPrefab.GetComponent<RectTransform>();
-            float halfWidth = rect.sizeDelta.x / 2f;
-            offset -= Vector3.right * halfWidth;
+            RectTransform prefabRect = keyTextPrefab.GetComponent<RectTransform>();
+            float halfWidth = prefabRect.sizeDelta.x / 2f;
+            offset -= Vector2.right * halfWidth;
 
             GameObject keyGO = Instantiate(keyTextPrefab, offset, Quaternion.identity, parent);
-            offset += Vector3.right * halfWidth;
+            RectTransform rect = keyGO.GetComponent<RectTransform>();
+            rect.anchoredPosition = offset;
+            offset += Vector2.right * halfWidth;
 
             TMP_Text keyText = keyGO.GetComponent<TMP_Text>();
             keyText.text = recordedEvent.key_name_display;
@@ -68,8 +70,8 @@ namespace FightDojo
             if (text != null)
             {
                 text.color = recordedEvent.action_canonical == "press"
-                    ? new Color(0.0f, 0.7f, 0.0f, 1f)
-                    : new Color(0.0f, 0.7f, 0.0f, 0.7f);
+                    ? new Color(0.0f, 0.0f, 0.7f, 1f)
+                    : new Color(0.0f, 0.0f, 0.7f, 0.7f);
             }
         }
     }
