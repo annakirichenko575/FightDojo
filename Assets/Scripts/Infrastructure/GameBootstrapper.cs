2abdc3d28b152126daadc71eff58750a61fb68b8
TSentler <centner_dc@mail.ru>
2026-02-11 19:48:52 +0500

    Add Services

---
+++ Assets/Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.AssetManagement;
+using Services;
+using Services.Randomizer;
+using UnityEngine;
+
+namespace Infrastructure
+{
+  public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
+  {
+    private readonly AllServices _services;
+    
+    private void Awake()
+    {
+
+    }
+    
+    private void RegisterServices() 
+    {
+      _services.RegisterSingle<IRandomService>(new RandomService());
+      _services.RegisterSingle<IAssetProvider>(new AssetProvider());
+      
+    }
+  }
+}
