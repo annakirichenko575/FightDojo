0f0ef4d510166468672575f066b8ce072e5522ae
TSentler <centner_dc@mail.ru>
2026-04-09 10:41:23 +0500

    Add ScrollRectSync

---
+++ Assets/Scripts/UI/ScrollRectSync.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectSync : MonoBehaviour
+{
+  public ScrollRect editorScroll;
+  public ScrollRect inputScroll;
+
+  private bool isSyncing = false;
+
+  private void OnEnable()
+  {
+    editorScroll.onValueChanged.AddListener(OnEditorScrollChanged);
+    inputScroll.onValueChanged.AddListener(OnInputScrollChanged);
+  }
+
+  private void OnDisable()
+  {
+    editorScroll.onValueChanged.RemoveListener(OnEditorScrollChanged);
+    inputScroll.onValueChanged.RemoveListener(OnInputScrollChanged);
+  }
+
+  private void OnEditorScrollChanged(Vector2 value)
+  {
+    if (isSyncing) 
+      return;
+        
+    isSyncing = true; 
+    inputScroll.horizontalNormalizedPosition = value.x;
+    isSyncing = false;
+  }
+
+  private void OnInputScrollChanged(Vector2 value)
+  {
+    if (isSyncing) 
+      return;
+        
+    isSyncing = true;
+    editorScroll.horizontalNormalizedPosition = value.x;
+    isSyncing = false;
+  }}
