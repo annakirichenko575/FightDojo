1999a8e3853269df0ad36d991642ee7b41957847
TSentler <centner_dc@mail.ru>
2026-05-19 17:59:25 +0500

    fix open db folder

--- Assets/Scripts/UI/Database/Game/DbNameButton.cs
+++ Assets/Scripts/UI/Database/Game/DbNameButton.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Diagnostics;
 using System.IO;
 using FightDojo.Database;
 using Services;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using System.Runtime.InteropServices;
 
 namespace FightDojo.UI.Database.Game
 {
@@ -15,7 +17,7 @@ namespace FightDojo.UI.Database.Game
             OpenFolder(dbService.DatabasePath);
         }
         
-        private static void OpenFolder(string path)
+        private void OpenFolder(string path)
         {
             if (string.IsNullOrEmpty(path) || !File.Exists(path))
             {
@@ -23,10 +25,48 @@ namespace FightDojo.UI.Database.Game
                 return;
             }
 
-#if UNITY_EDITOR || UNITY_STANDALONE_WIN
-            //Process.Start(path);
-            Process.Start("Explorer.exe", @"/select,""" + path + "\"");
-#endif
+            path = path.Replace("/", "\\");
+            try
+            {
+                RevealInExplorer(path);
+                //Process.Start("Explorer.exe", @"/select,""" + path + "\"");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Не удалось открыть проводник: " + e.Message);
+            } 
+        }
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        private static extern int SHOpenFolderAndSelectItems(IntPtr pidlFolder, uint cidl, IntPtr[] apidl, uint dwFlags);
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        private static extern void SHParseDisplayName(
+            [MarshalAs(UnmanagedType.LPWStr)] string name,
+            IntPtr bindingContext, out IntPtr pidl, uint sfgaoIn, out uint psfgaoOut);
+
+        private void RevealInExplorer(string path)
+        {
+            path = path.Replace("/", "\\");
+            string folder = Path.GetDirectoryName(path);
+
+            // Получаем PIDL папки
+            SHParseDisplayName(folder, IntPtr.Zero, out IntPtr nativeFolder, 0, out _);
+            // Получаем PIDL файла
+            SHParseDisplayName(path, IntPtr.Zero, out IntPtr nativeFile, 0, out _);
+
+            Debug.Log($"nativeFolder: {nativeFolder}, nativeFile: {nativeFile}");
+
+            if (nativeFolder == IntPtr.Zero || nativeFile == IntPtr.Zero)
+            {
+                Process.Start(folder);
+                return;
+            }
+
+            SHOpenFolderAndSelectItems(nativeFolder, 1, new[] { nativeFile }, 0);
+
+            Marshal.FreeCoTaskMem(nativeFolder);
+            Marshal.FreeCoTaskMem(nativeFile);
         }
     }
 }
